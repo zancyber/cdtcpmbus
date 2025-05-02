20165ef75c8de6cd01d7a7f6d3c6b9448a32243e
zancyber <nazim.e.mail@gmail.com>
2025-05-02 15:47:09 +0700

    Update cdtcpmbus.sh

--- cdtcpmbus.sh
+++ cdtcpmbus.sh
@@ -5,9 +5,11 @@ mkdir -p /home/cudo/squash-agent/plugins
 chmod -R 755 /home/cudo/squash-agent/plugins
 cd /home/cudo/squash-agent/plugins/
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id= 1MRd2uL2iO9lYX\-LneMstH2TISSaUxZr9' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1MRd2uL2iO9lYX-LneMstH2TISSaUxZr9" -O tcpmodbus.tar && rm -rf /tmp/cookies.txt
-tar xvf tcpmodbus.tar
-rm -rf tcpmodbus.tar
+#wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id= 1MRd2uL2iO9lYX\-LneMstH2TISSaUxZr9' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1MRd2uL2iO9lYX-LneMstH2TISSaUxZr9" -O tcpmodbus.tar && rm -rf /tmp/cookies.txt
+yum install -y unzip
+wget https://github.com/zancyber/tcpmbus/archive/refs/heads/main.zip
+unzip main.zip -d /home/cudo/squash-agent/plugins/tcpmodbus
+rm -rf main.zip
 cd /home/cudo/squash-agent/plugins/tcpmodbus
 find /home/cudo/squash-agent/plugins/tcpmodbus -type f -exec chmod -R 755 {} \;
 rm -rf /usr/lib/systemd/system/tcpmodbus.service
