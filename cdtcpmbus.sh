09b596b1b2da67e4c50ddd2d7f13604bb6cf1e82
zancyber <nazim.e.mail@gmail.com>
2025-05-02 15:48:37 +0700

    Update cdtcpmbus.sh

--- cdtcpmbus.sh
+++ cdtcpmbus.sh
@@ -6,7 +6,7 @@ chmod -R 755 /home/cudo/squash-agent/plugins
 cd /home/cudo/squash-agent/plugins/
 
 #wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id= 1MRd2uL2iO9lYX\-LneMstH2TISSaUxZr9' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1MRd2uL2iO9lYX-LneMstH2TISSaUxZr9" -O tcpmodbus.tar && rm -rf /tmp/cookies.txt
-yum install -y unzip
+yum install -y unzip jq
 wget https://github.com/zancyber/tcpmbus/archive/refs/heads/main.zip
 unzip main.zip -d /home/cudo/squash-agent/plugins/tcpmodbus
 rm -rf main.zip
